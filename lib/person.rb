4e1f8a3bcc1de57837ef3804fdeb24a5d439c99e
maquino96 <matthewaquino96@gmail.com>
2021-01-12 20:08:44 -0500

    commit tree f17f932a386325d67797cd6246cb47d93290f4b5

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+
+
+class Person
+
+    def initiliaze ( name, job)
+
+        @name = name
+        @job = job
+
+    end
+
+    def name= (new_name)
+        @name = new_name
+    end 
+
+    def job=(new_job)
+        @job = new_job
+    end 
+
+    def name
+        @name
+    end 
+
+    def job
+        @job
+    end 
+
+end
